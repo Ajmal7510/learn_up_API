03a639cb951e1f215562d222a42d93863ff89068
Ajmal <ajuajmal1009@gmail.com>
2024-11-08 10:13:07 +0530

    first commit nothing added just created template

---
+++ src/main/java/com/project/learnup/learnUpAPI/LearnUpApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.learnup.learnUpAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnUpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/learnup/learnUpAPI/LearnUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.learnup.learnUpAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
